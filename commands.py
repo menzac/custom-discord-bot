723a0788afceb4956895f538e9e6a020d4b87cbb
Vojtech_Jelinek <vojtin.j@gmail.com>
2016-01-02 23:48:03 +0100

    add stats_formatter

--- commands.py
+++ commands.py
@@ -1,13 +1,13 @@
 # -*- coding: utf-8 -*-
 
 from exceptions import ChatException
+import stats_formatter as stats_f
 
 def bodik(command, message, data):
     try:
-        if command[1] in members:
+        if command[1] in members and message.author.name.lower() != command[1]:
             data[command[1]] = data.get(command[1], {})
             data[command[1]]["bodik"] = data[command[1]].get("bodik", 0) + 1
-            client.send_message(message.channel, str(data.get(command[1], 0)))
         else:
            raise ChatException("Tento uživatel neexistuje")
     except IndexError:
@@ -16,9 +16,9 @@ def bodik(command, message, data):
 def stats(command, message, data):
     try:
         if command[1] in members:
-            data[command[1]] = data.get(command[1], {})
-            client.send_message(message.channel, str(data[command[1]].get("bodik", 0)))
+            client.send_message(message.channel, stats_f.get_stats_for_user(data, command[1]))
         else:
             raise ChatException("Tento uživatel neexistuje")
     except IndexError:
-        raise ChatException("Potřebuju jméno či statistiky mám zobrazit")
+        stats_f.members = members
+        client.send_message(message.channel, stats_f.get_all_stats(data))
---
+++ stats_formatter.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+def get_all_stats(data):
+    text = "```python\n"
+    for user in members:
+        text += get_user_stats(data, user)
+    text += "```"
+    return text
+
+def get_stats_for_user(data, user):
+    text = "```python\n"
+    text += get_user_stats(data, user)
+    text += "```"
+    return text
+
+def get_user_stats(data, user):
+    data[user] = data.get(user, {})
+    text = user + ":\n"
+    for key in data[user].keys():
+       text += "   " str(key) + " = " + str(data[user][key]) + "\n"
+    text += "\n"
+    return text
